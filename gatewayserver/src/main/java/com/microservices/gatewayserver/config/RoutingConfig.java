823fccae09f7d334a2b24a194d22dbf30c5818ca
hamza dbani <hamzadbani1@gmail.com>
2023-06-20 20:57:23 +0200

    add security using OAuth2 authorization code grant flow

--- gatewayserver/src/main/java/com/microservices/gatewayserver/config/RoutingConfig.java
+++ gatewayserver/src/main/java/com/microservices/gatewayserver/config/RoutingConfig.java
@@ -1,5 +1,7 @@
 package com.microservices.gatewayserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -10,23 +12,29 @@ import java.util.Date;
 @Configuration
 public class RoutingConfig {
 
+    @Autowired
+    private TokenRelayGatewayFilterFactory filterFactory;
+
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(p -> p
                         .path("/microservices/accounts/**")
-                        .filters(f -> f.rewritePath("/microservices/accounts/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .filters(f -> f.filters(filterFactory.apply())
+                                .rewritePath("/microservices/accounts/(?<segment>.*)","/${segment}")
+                                .removeRequestHeader("Cookie"))
                         .uri("lb://ACCOUNTS")).
                 route(p -> p
                         .path("/microservices/loans/**")
-                        .filters(f -> f.rewritePath("/microservices/loans/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .filters(f -> f.filters(filterFactory.apply())
+                                .rewritePath("/microservices/loans/(?<segment>.*)","/${segment}")
+                                .removeRequestHeader("Cookie"))
                         .uri("lb://LOANS")).
                 route(p -> p
                         .path("/microservices/cards/**")
-                        .filters(f -> f.rewritePath("/microservices/cards/(?<segment>.*)","/${segment}")
-                                .addResponseHeader("X-Response-Time",new Date().toString()))
+                        .filters(f -> f.filters(filterFactory.apply())
+                                .rewritePath("/microservices/cards/(?<segment>.*)","/${segment}")
+                                .removeRequestHeader("Cookie"))
                         .uri("lb://CARDS")).build();
     }
 
